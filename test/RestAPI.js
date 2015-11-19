2e40dea2a751dc104126eff69639685dc68a3668
Francis Wertz <francis.wertz@seisan.com>
2015-11-18 22:39:54 -0500

    Plivo requests return promises

--- test/RestAPI.js
+++ test/RestAPI.js
@@ -70,12 +70,25 @@ describe('RestAPI', function() {
                 .reply(204, "");
 
         it('should treat params as callback when params are not provided and optional is true.', function(done) {
-            rest.get_cdrs(function(status, response) {
-                assert.equal(200, status);
-                assert.equal('object', typeof response);
-
+            // rest.get_cdrs(function(status, response) {
+            //     assert.equal(200, status);
+            //     assert.equal('object', typeof response);
+            //
+            //     done();
+            // });
+            rest
+              .get_cdrs()
+              .then( function ( response ) {
+                assert.equal( 'object', typeof response );
+                assert.equal( 200, response.statusCode )
                 done();
-            });
+              })
+              .then( null, function( err ) {
+                  done( err );
+              })
+              .catch( function ( ex ) {
+                done( ex );
+              });
         });
 
         it('should continue when callback is not provided.', function() {
@@ -89,38 +102,81 @@ describe('RestAPI', function() {
         });
 
         it('should successfully send GET requests with parameters.', function(done) {
-            rest.get_cdrs({
-                limit: 5,
-            }, function(status, response) {
-                // if 200 is returned, then nock successfully found the right url
-                assert.equal(200, status);
-                assert.equal('object', typeof response);
-
+            // rest.get_cdrs({
+            //     limit: 5,
+            // }, function(status, response) {
+            //     // if 200 is returned, then nock successfully found the right url
+            //     assert.equal(200, status);
+            //     assert.equal('object', typeof response);
+            //
+            //     done();
+            // });
+            rest
+              .get_cdrs( {limit: 5} )
+              .then( function ( response ) {
+                assert.equal( 'object', typeof response );
+                assert.equal( 200, response.statusCode )
                 done();
-            });
+              })
+              .then( null, function( err ) {
+                  done( err );
+              })
+              .catch( function ( ex ) {
+                done( ex );
+              })
         });
 
         it('should successfully send POST requests.', function(done) {
-            rest.make_call({
+            // rest.make_call({
+            //     answer_url: 'http://test.com',
+            //     to: '1234567890',
+            //     from: '1234567890',
+            // }, function(status, response) {
+            //     assert.equal(201, status);
+            //     assert.equal('object', typeof response);
+            //
+            //     done();
+            // });
+
+            rest
+              .make_call({
                 answer_url: 'http://test.com',
                 to: '1234567890',
-                from: '1234567890',
-            }, function(status, response) {
-                assert.equal(201, status);
-                assert.equal('object', typeof response);
-
+                from: '1234567890'
+              })
+              .then( function ( response ) {
+                assert.equal( 'object', typeof response );
+                assert.equal( 201, response.statusCode );
                 done();
-            });
+              })
+              .then( null, function( err ) {
+                  done( err );
+              })
+              .catch( function ( ex ) {
+                done( ex );
+              })
         });
 
         it('should successfully send DELETE requests.', function(done) {
-            rest.hangup_all_calls(function(status, response) {
-                assert.equal(204, status);
-                assert.equal('string', typeof response);
-                assert.equal('', response);
-
+            // rest.hangup_all_calls(function(status, response) {
+            //     assert.equal(204, status);
+            //     assert.equal('string', typeof response);
+            //     assert.equal('', response);
+            //
+            //     done();
+            // });
+            rest
+              .hangup_all_calls()
+              .then( function ( response ) {
+                assert.equal( 204, response.statusCode )
                 done();
-            });
+              })
+              .then( null, function( err ) {
+                  done( err );
+              })
+              .catch( function ( ex ) {
+                done( ex );
+              })
         });
     });
 
@@ -133,21 +189,21 @@ describe('RestAPI', function() {
 		it('should match signature of plain text', function(done) {
 			var params = { 'ascii': ' !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~' };
 			var test_signature = rest.create_signature('https://' + rest.options.host, params);
-		
+
 			assert.equal('pNTHTayG6CHS3s2c7AbsJUYIrno=', test_signature);
 
 			done();
-	
+
 		});
 
 		it('should match signature of utf-8 text', function(done) {
 			var params = { 'utf': '\xC4\xA4\xC4\x98\xC4\xBD\xC4\xBF\xC5\x8C\xC2\xA0\xE1\xBA\x80\xCE\xB8\xC5\x94\xC4\xBD\xE1\xB8\x8A\xE2\x80\xA6\xC2\xA9\xF0\x9D\x8C\x86\xE2\x98\x83\xF0\x9F\x98\x80\xF0\x9F\x98\x81\xF0\x9F\x98\x82\xF0\x9F\x98\x83\xF0\x9F\x98\x84\xF0\x9F\x98\x85\xF0\x9F\x98\x86\xF0\x9F\x98\x87' };
 			var test_signature = rest.create_signature('https://' + rest.options.host, params);
-	
+
 			assert.equal('0gQVBqlj63pjoVO6dHmGJsEajS4=', test_signature);
 
 			done();
-		
+
 		});
 	});
 
