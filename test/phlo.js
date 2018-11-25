dbd845762f46e1b7f9bc623ae58510ae68db8488
Ravi <ravipatel.ce@gmail.com>
2018-11-25 10:36:14 +0530

    Added test cases for multiparty call.

--- test/phlo.js
+++ test/phlo.js
@@ -1,22 +1,37 @@
 'use strict';
 
-import { PhloClient } from '../lib/rest/client';
+import {
+  PhloClient
+} from '../lib/rest/client';
 
 describe('phlo client init', function () {
-    it('Should initialize phlo via phloClient.phlo.get()', function () {
+  it('Should initialize phlo via phloClient.phlo.get()', function () {
 
-        let phloClient = new PhloClient('Auth Id', 'Auth Token');
-        console.log('===>', phloClient.phlo(12));
-        // console.log('===>', phloClient.phlo.get(12));
-        // phloClient.print('lksdjflkdsjfklsfj');
-        // console.log(' ==>', phloClient.phlo);
-        return true;
+    let phloClient = new PhloClient('Auth Id', 'Auth Token');
+    let phlo = phloClient.phlo(12);
+    return true;
+  });
+
+  it('Should initialize phlo via phloClient.phlo()', function () {
+
+    let phloClient = new PhloClient('Auth Id', 'Auth Token');
+    let phlo = phloClient.phlo(12);
+    return true;
+  });
+
+  it('Initiate multiparty call', function () {
+
+    let phloClient = new PhloClient('Auth Id', 'Auth Token');
+    phloClient.phlo(12).multiPartyCall(21).call('9920700964', '9920700964').then((result) => {
+      return true;
+    }).catch((err) => {
+      return false;
     });
 
-    it('Should initialize phlo via phloClient.phlo()', function () {
-
-        let phloClient = new PhloClient('Auth Id', 'Auth Token');
-        console.log('===>', phloClient.phlo(12));
-        return true;
-    });
+    // console.log('===>', phloClient.phlo(12));
+    // console.log('===>', phloClient.phlo.get(12));
+    // phloClient.print('lksdjflkdsjfklsfj');
+    // console.log(' ==>', phloClient.phlo);
+    return true;
+  });
 });
