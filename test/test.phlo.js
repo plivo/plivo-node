d1c49eede5d8891e97b691733989f350ec7b4b79
patelravi <ravipatel.ce@gmail.com>
2019-01-25 13:55:35 +0530

    Added test proxy for test cases.

--- test/test.phlo.js
+++ test/test.phlo.js
@@ -2,45 +2,53 @@
 
 import {
   PhloClient
-} from '../lib/rest/client';
+} from '../lib/rest/client-test';
+// } from '../lib/rest/client';
+import { doesNotReject } from 'assert';
 
-describe('phlo client init', function () {
+let authId = 'MAZJJKMWNLZJNIYJKYYT';
+let authToken = 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw';
+let phloId = 'b30083e9-73c9-42a2-acfa-c08e6e66cd83';
+let mpcId = '85169eaf-d8b9-4e3d-9baf-13eb6b231bb0';
 
-  let authId = 'MAZJJKMWNLZJNIYJKYYT';
-  let authToken = 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw';
-  let phloId = 'b30083e9-73c9-42a2-acfa-c08e6e66cd83';
+describe('PhloInterface', function () {
 
-  /******************** Phlo resource test cases *********************/
-
-  it('Should initialize phlo via phloClient.phlo.get()', async function () {
+  it('Should initialize phlo via phloClient.phlo.get(phloId)', async function () {
 
     try {
 
       let phloClient = new PhloClient('MAZJJKMWNLZJNIYJKYYT', 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw');
-      let phlo = await phloClient.phlo.get('b30083e9-73c9-42a2-acfa-c08e6e66cd83');
+      let phloResult = await phloClient.phlo.get('b30083e9-73c9-42a2-acfa-c08e6e66cd83');
+
+      //console.log('phlo result is ', phloResult);
+
       return true;
     } catch (err) {
-      return err;
+      throw err;
     }
   });
 
-  it('Should initialize phlo via phloClient.phlo()', async function () {
+  it('Should initialize phlo via phloClient.phlo(phloId)', async function () {
 
     let phloClient = new PhloClient('MAZJJKMWNLZJNIYJKYYT', 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw');
     let phlo = await phloClient.phlo('b30083e9-73c9-42a2-acfa-c08e6e66cd83');
+    // console.log('init result', phlo);
     return true;
   });
 
-  it('Run phlo via phloClient.phlo()', async function () {
+  it('Run phlo via phloClient.phlo(phloId).run()', async function () {
 
     let phloClient = new PhloClient('MAZJJKMWNLZJNIYJKYYT', 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw');
     let phlo = await phloClient.phlo('b30083e9-73c9-42a2-acfa-c08e6e66cd83').run();
+    // console.log('phlo run result', phlo);
     return true;
   });
+});
 
-  /******************** Multiparty call test cases *********************/
 
-  let mpcId = '85169eaf-d8b9-4e3d-9baf-13eb6b231bb0';
+/******************** Multiparty call test cases *********************/
+
+describe('PhloMultiPartyCallInterface', function () {
 
   it('Get multiparty call details', async function () {
     let phloClient = new PhloClient('MAZJJKMWNLZJNIYJKYYT', 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw');
@@ -53,33 +61,26 @@ describe('phlo client init', function () {
 
     let phloClient = new PhloClient(authId, authToken);
     let phlo = phloClient.phlo(phloId);
-    let result = await phlo.multiPartyCall(mpcId).call('919920700964', '919620074923');
+    let result = await phlo.multiPartyCall(mpcId).call('919920700964', '919898967510');
     return true;
-
   });
 
   it('Warm Transfer - multi party call', async function () {
     let phloClient = new PhloClient(authId, authToken);
     let phlo = phloClient.phlo(phloId);
-    let result = await phlo.multiPartyCall(mpcId).warmTransfer('919920700964', '919620074923');
-    return true;
+    await phlo.multiPartyCall(mpcId).warmTransfer('919920700964', '919898967510');
   });
 
   it('Cold Transfer - multi party call', async function () {
     let phloClient = new PhloClient(authId, authToken);
     let phlo = phloClient.phlo(phloId);
     let result = await phlo.multiPartyCall(mpcId).coldTransfer('919920700964', '919898967510');
-    return true;
   });
 
   it('Abort Transfer - multi party call', async function () {
-    setTimeout(async () => {
-      let phloClient = new PhloClient(authId, authToken);
-      let phlo = phloClient.phlo(phloId);
-      let result = await phlo.multiPartyCall(mpcId).abortTransfer('919920700964');
-    }, 2000);
-    return true;
-
+    let phloClient = new PhloClient(authId, authToken);
+    let phlo = phloClient.phlo(phloId);
+    await phlo.multiPartyCall(mpcId).abortTransfer('919920700964');
   });
 
 });
---
+++ test/test.phloMpcMember.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import {
+  PhloClient
+} from '../lib/rest/client-test';
+// } from '../lib/rest/client';
+
+describe('Phlo Member Interface', function () {
+
+  let authId = 'MAZJJKMWNLZJNIYJKYYT';
+  let authToken = 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw';
+  let phloId = 'b30083e9-73c9-42a2-acfa-c08e6e66cd83';
+  let nodeId = '85169eaf-d8b9-4e3d-9baf-13eb6b231bb0';
+
+  let memberAddress = '919920700964';
+
+  /******************** Phlo resource test cases *********************/
+
+  it('resumeCall - Phlo Member', async function () {
+    let phloClient = new PhloClient(authId, authToken);
+    await phloClient.phlo(phloId).multiPartyCall(nodeId).member(memberAddress).resumeCall();
+  });
+
+  it('voicemail drop - Phlo Member', async function () {
+    let phloClient = new PhloClient(authId, authToken);
+    await phloClient.phlo(phloId).multiPartyCall(nodeId).member(memberAddress).voicemailDrop();
+  });
+
+  it('hangup - Phlo Member', async function () {
+    let phloClient = new PhloClient(authId, authToken);
+    await phloClient.phlo(phloId).multiPartyCall(nodeId).member(memberAddress).hangup();
+  });
+
+  it('hold - Phlo Member', async function () {
+    let phloClient = new PhloClient(authId, authToken);
+    await phloClient.phlo(phloId).multiPartyCall(nodeId).member(memberAddress).hold();
+  });
+
+  it('unhold - Phlo Member', async function () {
+    let phloClient = new PhloClient(authId, authToken);
+    await phloClient.phlo(phloId).multiPartyCall(nodeId).member(memberAddress).unhold();
+  });
+
+});
