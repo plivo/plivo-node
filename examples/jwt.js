85eba2254696883747e9209dd3ef9ef951e50c0b
Kritarth <kritarth@plivo.com>
2020-05-21 15:09:03 +0530

    example update

--- examples/jwt.js
+++ examples/jwt.js
@@ -1,12 +1,15 @@
 var Plivo =  require('../dist/rest/client.js');
 
 // token generation using from epoch and length in seconds
-var acctkn = new Plivo.AccessToken('{authId}', '{authToken}', 'endpointUsername', {validFrom: (new Date()).getTime()/1000, lifetime: 300}, 'username-12345');
+var acctkn = new Plivo.AccessToken('{authId}', '{authToken}', '{endpointUsername}', {validFrom: (new Date()).getTime()/1000, lifetime: 300}, '{uid}');
+
+// voice (incoming, outgoing) grants
+acctkn.addVoiceGrants(false, true);
 console.log(acctkn.toJwt());
 
 
 // token with validity of 24 hours starting now
-acctkn = new Plivo.AccessToken('{authId}', '{authToken}', 'endpointUsername', {}, 'uid');
+acctkn = new Plivo.AccessToken('{authId}', '{authToken}', '{endpointUsername}', {}, '{uid}');
 
 // voice (incoming, outgoing) grants
 acctkn.addVoiceGrants(true, true);
