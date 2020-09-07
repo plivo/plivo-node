ab352d5d0ea1abab0a53f6ee5c8e7e971abf4222
Koushik-Ayila <koushik.ayila@plivo.com>
2020-09-08 00:40:30 +0530

    MPC API and XML changes - Dev completed

--- example_script.js
+++ example_script.js
@@ -2,8 +2,14 @@ let plivo = require('./');
 var client = new plivo.Client('MAMDJMMTEZOWY0ZMQWM2', 'OTljNmVmOGVkNGZhNjJlOWIyMWM0ZDI0ZjQwZDdk');
 
 // client.calls.create( "+919090909090", // from
-//   "sip:koushikqa119062465586783372208@phone-qa.voice.plivodev.com", // to
+//   "+918309866821", // to
 //   "https://plivobin.non-prod.plivops.com/api/v1/Conference_test07.xml").then(function (response) {
+//   console.log(response);
+// }, function (err) {
+//   console.error(err);
+// });
+//
+// client.conferences.get( "FStestconference").then(function (response) {
 //   console.log(response);
 // }, function (err) {
 //   console.error(err);
@@ -34,13 +40,24 @@ var client = new plivo.Client('MAMDJMMTEZOWY0ZMQWM2', 'OTljNmVmOGVkNGZhNjJlOWIyM
 //   console.error(err);
 // });
 
-client.multiPartyCalls.addParticipant('agent', {'friendlyName' : 'TestMPC', 'from' : '+919090909090', 'to': 'sip:koushikqa119062465586783372208@phone-qa.voice.plivodev.com'}).then(function (response) {
-  console.log(response);
-}, function (err) {
-  console.error(err);
-});
+// client.multiPartyCalls.stop('6172e321-284d-4159-8242-5f5e92933de5').then(function (response) {
+//   console.log(response);
+// }, function (err) {
+//   console.error(err);
+// });
 
-// client.multiPartyCalls.list().then(function (response) {
+// client.multiPartyCalls.kickParticipant(10, "6bed1e3a-e5da-4ffc-8ed5-84b0a3e0d75c", null).then(function (response) {
+//   console.log(response);
+// }, function (err) {
+//   console.error(err);
+// });
+// client.multiPartyCalls.get(null, 'TestMPC').then(function (response) {
+//   console.log(response);
+// }, function (err) {
+//   console.error(err);
+// });
+
+// client.multiPartyCalls.get('6f84d47c-ee82-4172-a155-c6e22f87d874').then(function (response) {
 //   console.log(response);
 // }, function (err) {
 //   console.error(err);
