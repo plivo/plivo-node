ed1b91a31b227a7ef351d8f84c537dc21a942d87
Huzaif <huzaif@Huzaifs-MacBook-Pro.local>
2022-07-29 00:31:36 +0530

    coverage check

--- test/accounts.js
+++ test/accounts.js
@@ -81,6 +81,13 @@ describe('Account', function () {
       })
   });
 
+  it('should throw error for update subAccount via interface', function () {
+    return client.subAccounts.update()
+        .catch(function(errorResponse) {
+        assert.equal(errorResponse, 'Error: Missing mandatory field: id')
+      })
+  });
+
   it('list subAccounts', function () {
     return client.subAccounts.list()
       .then(function(accounts) {
