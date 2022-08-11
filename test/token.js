b8b1d5407c48e1a4cb8e888d370b9d3afa8ce3a4
kowshik-plivo <intern-kowshik@plivo.com>
2022-08-12 03:41:37 +0530

    Changes in token creation

--- test/token.js
+++ test/token.js
@@ -3,13 +3,7 @@ import {Client} from '../lib/rest/client-test';
 
 let client = new Client('sampleid', 'sammpletoken', 'sampleproxy');
 
-describe('Token', function () {
-  it('should get Token', function () {
-    return client.token.get(1)
-      .then(function(token) {
-        assert.equal(token.app_id, 1)
-      })
-  });
+describe('JWT/Token', function () {
   it('should create endpoint via interface', function () {
     return client.token.create('sampleid')
       .then(function(token){
