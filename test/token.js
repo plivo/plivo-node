6f16819af84fcd342e20c5d46ec286bde052cc36
kowshik-plivo <intern-kowshik@plivo.com>
2022-08-11 13:10:25 +0530

    Changes in unit testing files

---
+++ test/token.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import {Client} from '../lib/rest/client-test';
+
+let client = new Client('sampleid', 'sammpletoken', 'sampleproxy');
+
+describe('Token', function () {
+  it('should create endpoint via interface', function () {
+    return client.token.create('sampleid')
+      .then(function(token){
+        assert.equal(token.token, 'created')
+      })
+  });
+});
