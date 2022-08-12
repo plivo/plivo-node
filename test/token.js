e840af1fb5c920c90249312f0d459b3a1577fc4f
kowshik-plivo <intern-kowshik@plivo.com>
2022-08-12 10:37:44 +0530

    Changes in token creation

--- test/token.js
+++ test/token.js
@@ -8,7 +8,7 @@ let client = new Client('sampleid', 'sammpletoken', 'sampleproxy');
 
 describe('Token', function () {
   it('should create token via interface', function () {
-    return client.createToken().then(function (response) {
+    return client.token.then(function (response) {
       assert.equal(response.apiid, '5cbad7b4-19f4-11ed-8b03-0242ac110005');
       assert.equal(response.token, 'eyJhbGciOiJIUzI1NiIsImN0eSI6InBsaXZvO3Y9MSIsInR5cCI6IkpXVCJ9.eyJhcHAiOiIiLCJleHAiOjE2NjAzNjM2ODMsImlzcyI6Ik1BTURWTFpKWTJaR1k1TVdVMVpKIiwibmJmIjoxNjYwMjc3MjgzLCJwZXIiOnsidm9pY2UiOnsiaW5jb21pbmdfYWxsb3ciOmZhbHNlLCJvdXRnb2luZ19hbGxvdyI6dHJ1ZX19LCJzdWIiOiIifQ.LAwFEuotTmbZeGWBhfNT4X2KbRapYF23BrkwVfmr5A4');
     }).catch(function (error) {
