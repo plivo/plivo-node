8b616c41ffa37e1b1b66731c3777754fb8513842
mohsin-plivo <mohsin.hafiz@plivo.com>
2023-01-12 15:03:29 +0530

    Dockerisation for local development

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:18.04
+
+WORKDIR /usr/src/app
+RUN apt-get update && apt-get install -y wget git vim
+
+# Install node using nvm
+RUN mkdir -p /usr/src/.nvm
+ENV NVM_DIR /usr/src/.nvm
+RUN wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | sh
+ENV NODE_VERSION v14.0.0
+RUN /bin/sh -c ". $NVM_DIR/nvm.sh && nvm install $NODE_VERSION"
+
+ENV NODE_PATH $NVM_DIR/versions/node/$NODE_VERSION/lib/node_modules
+ENV PATH      $NVM_DIR/versions/node/$NODE_VERSION/bin:$PATH
+
+# Copy setup script
+COPY setup_sdk.sh /usr/src/app/
+RUN chmod a+x /usr/src/app/setup_sdk.sh
+
+ENTRYPOINT [ "/usr/src/app/setup_sdk.sh" ]
---
+++ setup_sdk.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+testDir="node-sdk-test"
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+cd /usr/src/app
+
+echo "Setting plivo-api endpoint to dev..."
+find /usr/src/app/lib/rest -type f -exec sed -i "s/$PLIVO_API_PROD_HOST/$PLIVO_API_DEV_HOST/g" {} \;
+
+echo "Packaging SDK..."
+echo "npm install" && npm install
+echo "npm run prepublish" && npm run prepublish
+echo "npm pack | tail -n 1" && package=$( npm pack | tail -n 1 )
+
+if [ ! -d $testDir ]; then
+    echo "Creating test dir..."
+    mkdir -p $testDir
+fi
+
+if [ ! -f $testDir/test.js ]; then
+    echo "Creating test file..."
+    echo -e "let plivo = require('plivo')\n" > $testDir/test.js
+    echo -e "\nlet client = new plivo.Client(process.env.PLIVO_AUTH_ID, process.env.PLIVO_AUTH_TOKEN);" >> $testDir/test.js
+fi
+
+echo "Installing dependencies for testing..."
+mv $package $testDir 
+cd $testDir 
+rm -rf package*.json node_modules
+npm init -y
+npm install $package
+rm $package
+
+echo -e "\n\nSDK setup complete!"
+echo "To test your changes:"
+echo -e "\t1. Add your test code in <path_to_cloned_sdk>/$testDir/test.js on host (or /usr/src/app/$testDir/test.js in the container)"
+echo -e "\t2. Run a terminal in the container using: $GREEN docker exec -it $HOSTNAME /bin/bash$NC"
+echo -e "\t3. Navigate to the test directory: $GREEN cd /usr/src/app/$testDir$NC"
+echo -e "\t4. Run your test file: $GREEN node test.js$NC"
+echo -e "\t5. For running unit tests, run on host: $GREEN make test CONTAINER=$HOSTNAME$NC"
+
+/bin/bash
