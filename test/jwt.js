9dd79e3dec6f75790ea9fe068ef9d511fd43ca77
Prashant Pandey <prashant.pandey@plivo.com>
2023-02-28 17:10:10 +0530

    Test push

--- test/jwt.js
+++ test/jwt.js
@@ -19,11 +19,4 @@ describe('Jwt', function () {
     acctkn.addVoiceGrants(true, true);
     assert.equal(acctkn.toJwt(), 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InBsaXZvO3Y9MSJ9.eyJqdGkiOiJ1c2VybmFtZS0xMjM0NSIsImlzcyI6Ik1BREFEQURBREFEQURBREFEQURBIiwic3ViIjoidXNlcm5hbWUiLCJuYmYiOjEyMTIxMjEyLCJleHAiOjEyMTIxNTEyLCJncmFudHMiOnsidm9pY2UiOnsiaW5jb21pbmdfYWxsb3ciOnRydWUsIm91dGdvaW5nX2FsbG93Ijp0cnVlfX19.ilC13HrMls4w3UHWZsZNudAALgWaSBkkXmZZ-VnT6GU');
   });
-  it('should fail with validFrom, lifetime and validTill', function () {
-    try {
-      let acctkn = new AccessToken('MADADADADADADADADADA', 'qwerty', 'username', {validFrom: 12121212, lifetime: 300, validTill: 12121512}, 'username-12345');
-    } catch (e) {
-      assert.equal(e.toString(), "Error: Please define at maximum any two of validFrom, lifetime and validTill");
-    }
-  });
 });
