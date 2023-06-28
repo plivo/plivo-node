dc5d37eff52778934fda6178e9b9f98b58c5bd0c
Ethan Czahor <ethanczahor@gmail.com>
2023-06-28 11:49:11 -0700

    added new interfaces to client

--- types/rest/client.d.ts
+++ types/rest/client.d.ts
@@ -1,37 +1,51 @@
 export function Response(): any;
-export function validateV3Signature(method: string, uri: string, nonce: string, auth_token: string, v3_signature: string, params?: {}): Boolean;
-export function validateSignature(uri: string, nonce: string, signature: string, auth_token: string): Boolean;
+export function validateV3Signature(
+  method: string,
+  uri: string,
+  nonce: string,
+  auth_token: string,
+  v3_signature: string,
+  params?: {}
+): Boolean;
+export function validateSignature(
+  uri: string,
+  nonce: string,
+  signature: string,
+  auth_token: string
+): Boolean;
 /**
  * Plivo API client which can be used to access the Plivo APIs.
  * To set a proxy or timeout, pass in options.proxy (url) or options.timeout (number in ms)
  * You can also pass in additional parameters accepted by the node requests module.
  */
 export class Client {
-    constructor(authId?: string, authToken?: string, options?: string);
-    calls: CallInterface;
-    token: TokenInterface;
-    accounts: AccountInterface;
-    subaccounts: SubaccountInterface;
-    subAccounts: SubaccountInterface;
-    applications: ApplicationInterface;
-    conferences: ConferenceInterface;
-    endpoints: EndpointInterface;
-    messages: MessageInterface;
-    lookup: LookupInterface;
-    powerpacks: PowerpackInterface;
-    numbers: NumberInterface;
-    pricings: PricingInterface;
-    recordings: RecordingInterface;
-    callFeedback: CallFeedbackInterface;
-    media: MediaInterface;
-    endUsers: EndUserInterface;
-    complianceDocumentTypes: ComplianceDocumentTypeInterface;
-    complianceDocuments: ComplianceDocumentInterface;
-    complianceRequirements: ComplianceRequirementInterface;
-    complianceApplications: ComplianceApplicationInterface;
-    loa: LOAInterface;
-    hostedMessagingNumber: HostedMessagingNumberInterface;
-    toJSON(...args: any[]): any;
+  constructor(authId?: string, authToken?: string, options?: string);
+  calls: CallInterface;
+  token: TokenInterface;
+  accounts: AccountInterface;
+  subaccounts: SubaccountInterface;
+  subAccounts: SubaccountInterface;
+  applications: ApplicationInterface;
+  conferences: ConferenceInterface;
+  endpoints: EndpointInterface;
+  messages: MessageInterface;
+  lookup: LookupInterface;
+  powerpacks: PowerpackInterface;
+  numbers: NumberInterface;
+  pricings: PricingInterface;
+  recordings: RecordingInterface;
+  callFeedback: CallFeedbackInterface;
+  media: MediaInterface;
+  endUsers: EndUserInterface;
+  complianceDocumentTypes: ComplianceDocumentTypeInterface;
+  complianceDocuments: ComplianceDocumentInterface;
+  complianceRequirements: ComplianceRequirementInterface;
+  complianceApplications: ComplianceApplicationInterface;
+  loa: LOAInterface;
+  hostedMessagingNumber: HostedMessagingNumberInterface;
+  brand: BrandInterface;
+  campaign: CampaignInterface;
+  toJSON(...args: any[]): any;
 }
 /**
  * Plivo API client which can be used to access the Plivo APIs.
@@ -39,8 +53,8 @@ export class Client {
  * You can also pass in additional parameters accepted by the node requests module.
  */
 export class PhloClient {
-    constructor(authId: any, authToken: any, options: any);
-    phlo: (phloId: any) => Phlo;
+  constructor(authId: any, authToken: any, options: any);
+  phlo: (phloId: any) => Phlo;
 }
 import { CallInterface } from "../resources/call.js";
 import { TokenInterface } from "../resources/token.js";
@@ -60,8 +74,10 @@ import { MediaInterface } from "../resources/media.js";
 import { Phlo } from "../resources/phlo.js";
 import { EndUserInterface } from "../resources/endUsers";
 import { ComplianceDocumentTypeInterface } from "../resources/complianceDocumentTypes";
-import { ComplianceDocumentInterface} from "../resources/complianceDocuments";
+import { ComplianceDocumentInterface } from "../resources/complianceDocuments";
 import { ComplianceRequirementInterface } from "../resources/complianceRequirements";
 import { ComplianceApplicationInterface } from "../resources/complianceApplications";
 import { LOAInterface } from "../resources/loa";
 import { HostedMessagingNumberInterface } from "../resources/hostedMessagingNumber";
+import { BrandInterface } from "../resources/brand.js";
+import { CampaignInterface } from "../resources/campaign.js";
