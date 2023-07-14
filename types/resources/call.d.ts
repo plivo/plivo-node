07bd2a573f646733628502f96be692984e675964
abinaya-plivo <abinaya.s@plivo.com>
2023-07-14 18:04:57 +0530

    update,delete and list session

--- types/resources/call.d.ts
+++ types/resources/call.d.ts
@@ -33,6 +33,49 @@ export class GetMaskingSessionResponse {
 	apiId: string;
 	response: object;
 }
+export class DeleteMaskingSessionResponse {
+	constructor(params: object);
+	apiId: string;
+	message: string;
+}
+export class UpdateMaskingSessionResponse {
+	constructor(params: object);
+	apiId: string;
+	message: string;
+	session: object;
+}
+export class ListMaskingSessionResponse {
+	constructor(params: object);
+	apiId: string;
+	response: object;
+	firstParty: string;
+	secondParty: string;
+	virtualNumber: string;
+	status: string;
+	initiateCallToFirstParty: string;
+	sessionUuid: string;
+	callbackUrl: string;
+	callbackMethod: string;
+	createdAt: string;
+	updatedAt: string;
+	expiryAt: string;
+	duration: string;
+	sessionCreationAmount: string;
+	callTimeLimit: string
+	ringTimeout: string;
+	firstPartyPlayUrl: string;
+	secondPartyPlayUrl: string;
+	record: string;
+	recordFileFormat: string;
+	recordingCallbackUrl: string;
+	recordingCallbackMethod: string;
+	interaction: object;
+	totalCallAmount: string;
+	totalCallCount: string;
+	totalCallBilledDuration: string;
+	totalSessionAmount: string;
+	lastInteractionTime: string;
+}
 export class GetQueuedCallResponse {
 	constructor(params: object);
 	apiId: string;
@@ -353,6 +396,55 @@ export class CallInterface extends PlivoResourceInterface {
      * @fail {Error} returns Error
      */
 	createMaskingSession(firstParty: string, secondParty: string, params ? : {}): Promise < CreateMaskingSessionResponse > ;
+	/**
+	 * Update a masking session
+	 * @method
+	 * @param {string} sessionUuid - unique idenfier of a session
+	 * @param {object} params - optional params to update a session
+	 * @param {number} [params.sessionExpiry] - The duration in seconds for which the masking session will be active.
+	 * @param {number} [params.callTimeLimit] - The maximum duration in seconds for each call in the masking session.
+	 * @param {boolean} [params.record] - Indicates whether the calls in the masking session should be recorded.
+	 * @param {string} [params.recordFileFormat] - The file format for the recorded calls.
+	 * @param {string} [params.recordingCallbackUrl] - The URL to which the recording callback will be sent.
+	 * @param {string} [params.callbackUrl] - The URL to which the callback for the masking session will be sent.
+	 * @param {string} [params.callbackMethod] - The HTTP method for the callback request.
+	 * @param {number} [params.ringTimeout] - The duration in seconds for which the call will ring before being canceled.
+	 * @param {string} [params.firstPartyPlayUrl] - The URL to play audio to the first party when the call is established.
+	 * @param {string} [params.secondPartyPlayUrl] - The URL to play audio to the second party when the call is established.
+	 * @param {string} [params.recordingCallbackMethod] - The HTTP method for the recording callback request.
+	 * @returns {Promise<PlivoGenericResponse>} - Resolves to a PlivoGenericResponse object
+	 * @throws {Error} - Throws an error if the update masking session request fails
+	 */
+	updateMaskingSession(sessionUuid: string, params ? : {}): Promise < UpdateMaskingSessionResponse > ;
+	/**
+	 * List masking sessions with optional filters
+	 * @method
+	 * @param {object} filterParams - Optional filter parameters to list masking sessions
+	 * @param {string} [filterParams.firstParty] - The phone number or SIP endpoint of the first party.
+	 * @param {string} [filterParams.secondParty] - The phone number or SIP endpoint of the second party.
+	 * @param {string} [filterParams.virtualNumber] - The virtual number associated with the masking session.
+	 * @param {string} [filterParams.status] - The status of the masking session.
+	 * @param {string} [filterParams.createdTimeEquals] - The specific created time to filter sessions.
+	 * @param {string} [filterParams.createdTimeLessThan] - Filter sessions created before this time.
+	 * @param {string} [filterParams.createdTimeGreaterThan] - Filter sessions created after this time.
+	 * @param {string} [filterParams.createdTimeLessOrEqual] - Filter sessions created before or at this time.
+	 * @param {string} [filterParams.createdTimeGreaterOrEqual] - Filter sessions created after or at this time.
+	 * @param {string} [filterParams.expiryTimeEquals] - The specific expiry time to filter sessions.
+	 * @param {string} [filterParams.expiryTimeLessThan] - Filter sessions expiring before this time.
+	 * @param {string} [filterParams.expiryTimeGreaterThan] - Filter sessions expiring after this time.
+	 * @param {string} [filterParams.expiryTimeLessOrEqual] - Filter sessions expiring before or at this time.
+	 * @param {string} [filterParams.expiryTimeGreaterOrEqual] - Filter sessions expiring after or at this time.
+	 * @param {number} [filterParams.durationEquals] - The duration in seconds to filter sessions.
+	 * @param {number} [filterParams.durationLessThan] - Filter sessions with duration less than this value.
+	 * @param {number} [filterParams.durationGreaterThan] - Filter sessions with duration greater than this value.
+	 * @param {number} [filterParams.durationLessOrEqual] - Filter sessions with duration less than or equal to this value.
+	 * @param {number} [filterParams.durationGreaterOrEqual] - Filter sessions with duration greater than or equal to this value.
+	 * @param {number} [filterParams.limit] - The maximum number of sessions to retrieve.
+	 * @param {number} [filterParams.offset] - The offset for paginated results.
+	 * @returns {Promise<PlivoGenericResponse>} - Resolves to a PlivoGenericResponse object
+	 * @throws {Error} - Throws an error if the list masking sessions request fails
+	 */
+	listMaskingSession(params ? : {}): Promise < ListMaskingSessionResponse > ;	
 
 	/**
      * Get a masking session
@@ -362,6 +454,14 @@ export class CallInterface extends PlivoResourceInterface {
      * @fail {Error} returns Error
      */
 	getMaskingSession(sessionUuid: string): Promise < GetMaskingSessionResponse > ;
+	/**
+     * Delete a masking session
+     * @method
+	 * @param {string} sessionUuid - unique idenfier of a session
+	 * @promise {object} returns PlivoGenericResponse Object
+     * @fail {Error} returns Error
+     */
+	deleteMaskingSession(sessionUuid: string): Promise < DeleteMaskingSessionResponse > ;
 	
 	/**
 	 * Hangup A Specific Call
